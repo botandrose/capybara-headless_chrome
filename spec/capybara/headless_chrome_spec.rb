a3202ec75cc76c83062ff351c51ad299a9a87b94
Micah Geisel <micah@botandrose.com>
2019-04-01 12:41:03 -0700

    switch to md5sum; hopefully that's on travis.

--- spec/capybara/headless_chrome_spec.rb
+++ spec/capybara/headless_chrome_spec.rb
@@ -2,13 +2,13 @@ RSpec.describe Capybara::HeadlessChrome do
   it "can download files", type: :feature do
     visit "/"
     click_link "Download document.pdf"
-    actual = crc32(page.downloads["document.pdf"].path)
-    expected = crc32("spec/support/document.pdf")
+    actual = md5sum(page.downloads["document.pdf"].path)
+    expected = md5sum("spec/support/document.pdf")
     expect(actual).to eq expected
   end
 
-  def crc32 path
-    `crc32 #{path}`.chomp.tap do
+  def md5sum path
+    `md5sum #{path} | cut -f1 -d' '`.chomp.tap do
       raise unless $?.success?
     end
   end
