08c4a5a9a9423f4216c8022c51e4c372f6d7c1c1
Micah Geisel <micah@botandrose.com>
2021-11-10 09:22:49 +0100

    only try to run driver-specific cucumber hooks if we're actually using this driver at the time.

--- lib/capybara/headless_chrome/cucumber.rb
+++ lib/capybara/headless_chrome/cucumber.rb
@@ -1,17 +1,21 @@
 Before do
-  page.downloads.reset
+  if page.driver.class.name == "Capybara::HeadlessChrome::Driver"
+    page.downloads.reset
+  end
 end
 
 AfterStep do
-  aggregate_failures 'javascript errors' do
-    page.javascript_errors.each do |error|
-      fail error
+  if page.driver.class.name == "Capybara::HeadlessChrome::Driver"
+    aggregate_failures 'javascript errors' do
+      page.javascript_errors.each do |error|
+        fail error
+      end
     end
-  end
 
-  page.javascript_warnings.each do |warning|
-    STDERR.puts 'WARN: javascript warning'
-    STDERR.puts warning
+    page.javascript_warnings.each do |warning|
+      STDERR.puts 'WARN: javascript warning'
+      STDERR.puts warning
+    end
   end
 end
 
